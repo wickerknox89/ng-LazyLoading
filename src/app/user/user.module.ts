23cfb8bc166294a6b1f246872d979875da4658a5
wickerknox <wickerknox89@gmail.com>
2024-10-21 15:50:54 -0700

    Register login, listing components in user module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
+import { ListingComponent } from './listing/listing.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    ListingComponent
+  ],
   imports: [
     CommonModule,
     UserRoutingModule
