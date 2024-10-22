bd7d67895305f1cc7a7f02dcf4c866e00642250a
wickerknox <wickerknox89@gmail.com>
2024-10-21 17:10:02 -0700

    user module - Add routes for login, listing components

--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { ListingComponent } from './listing/listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'listing', component:ListingComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
